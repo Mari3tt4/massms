be8021a0c4cc701c66baa20d0037b2006d7a69f6
Marietta <125894364+Mari3tt4@users.noreply.github.com>
2023-04-17 11:35:26 +0800

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./dist/*.{html,js}"],
+  theme: {
+    fontFamily:
+    {
+      poppins: "'Poppins','serif'"
+    },
+    fontWeight:
+    {
+      xb: 800,
+      b: 700,
+      sb: 600,
+      m: 500,
+      r: 400,
+      l: 300,
+      xlt: 200
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
